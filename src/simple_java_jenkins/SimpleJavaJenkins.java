c037d2193fb80f42be1db59b2582bdceb2614149
manipalreddy1747 <manipalreddy1747@gmail.com>
2023-04-11 08:30:54 +0530

    Initial commit

---
+++ src/simple_java_jenkins/SimpleJavaJenkins.java
@@ -0,0 +1,13 @@
+package simple_java_jenkins;
+
+public class SimpleJavaJenkins {
+
+	public static void main(String[] args) throws InterruptedException {
+		// TODO Auto-generated method stub
+		for (int i = 0; i < 10; i++) {
+			Thread.sleep(1500);
+			System.out.println("Hello World::" + i);
+		}
+	}
+
+}
